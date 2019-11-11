0986d83ea1678d348e11ad22675c000c1354562e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2019-11-11 08:28:52 -0400

    Initial commint

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Blue MFH',
+    'version': '13.0.1.0.0',
+    'author': 'Ynext SpA',
+    'maintainer': 'Leonardo J. Caballero G.',
+    'website': 'http://www.ynext.cl',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'summary': """
+        This module inherit from stock app the product.template model 
+        and add a new customizations for the 'Blue MFH'.""",
+
+    'description': """
+        The 'Blue MFH' module's purpose is...
+    """,
+    'depends': ['base', 'stock'],
+    'data': [
+        'views/views.xml',
+    ],
+    'images': ['static/description/icon.png'],
+}
